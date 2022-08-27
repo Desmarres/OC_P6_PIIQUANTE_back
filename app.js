db55bab6e8fbfe6f7067d75e9449fec4bb83dddc
Desmarres Sébastien <desmarres.sebastien@gmail.com>
2022-08-27 16:17:32 +0200

    Correction : problème auth

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const { connect } = require('./middleware/db.config');
-const auth = require('./middleware/sauce.req.auth');
 
 /* fichiers routes des requêtes*/ 
 const userRoutes = require('./routes/users.routes');
@@ -28,7 +27,7 @@ app.use((req, res, next) => {
 
 /* routage des requêtes */
 app.use('/api/auth/',userRoutes);
-app.use('/api/sauces/',auth,sauceRoutes);// contrôle de la validité de l'authentification avant le routage
+app.use('/api/sauces/',sauceRoutes);// contrôle de la validité de l'authentification avant le routage
 app.use('/resources/images', express.static(path.join(__dirname, 'resources/images')));
 
 /* export de la constante d'application express*/
--- routes/sauces.routes.js
+++ routes/sauces.routes.js
@@ -1,34 +1,35 @@
 /* importation des ressources*/
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/sauce.req.auth');
 const multer = require('../middleware/multer.config');
 const validation = require('../middleware/sauce.req.validation');
 const validationResult = require('../middleware/validationResult');
 const sauceCtrl = require('../controllers/sauces.controllers');
 
 /* routage des requêtes */
-router.get('/',sauceCtrl.getAllSauce); // accès au controleur de la requête
+router.get('/',auth,sauceCtrl.getAllSauce); // accès au controleur de la requête
 
-router.post('/',multer, // vérification de la conformité du fichier et stockage
+router.post('/',auth,multer, // vérification de la conformité du fichier et stockage
                 validation.sauce, // vérification des éléments descriptifs de la sauce
                 validationResult, // vérification de la présence d'erreur dans les entrées
                 sauceCtrl.createSauce); // accès au controleur de la requête
 
-router.get('/:id', validation.idSauce, // vérification de la validité de l'id sauce de l'URL
+router.get('/:id',auth, validation.idSauce, // vérification de la validité de l'id sauce de l'URL
                 validationResult, // vérification de la présence d'erreur dans les entrées
                 sauceCtrl.getOneSauce); // accès au controleur de la requête
 
-router.put('/:id',validation.idSauce, // vérification de la validité de l'id sauce de l'URL
+router.put('/:id',auth,validation.idSauce, // vérification de la validité de l'id sauce de l'URL
                 multer, // vérification de la conformité du fichier et stockage
                 validation.sauce, // vérification des éléments descriptifs de la sauce
                 validationResult, // vérification de la présence d'erreur dans les entrées
                 sauceCtrl.modifySauce); // accès au controleur de la requête
 
-router.delete('/:id',validation.idSauce, // vérification de la validité de l'id sauce de l'URL
+router.delete('/:id',auth,validation.idSauce, // vérification de la validité de l'id sauce de l'URL
                 validationResult, // vérification de la présence d'erreur dans les entrées
                 sauceCtrl.deleteSauce); // accès au controleur de la requête
 
-router.post('/:id/like',validation.idSauce, // vérification de la validité de l'id sauce de l'URL
+router.post('/:id/like',auth,validation.idSauce, // vérification de la validité de l'id sauce de l'URL
                 validation.managementSauceLike, // vérification des éléments descriptifs de la sauce
                 validationResult, // vérification de la présence d'erreur dans les entrées
                 sauceCtrl.managementSauceLike); // accès au controleur de la requête
